77f500305802723edf0375fcca6b1271ece25082
Alex <108566298+okay-Alex@users.noreply.github.com>
2025-04-07 19:46:31 -0700

    Update LogicOperators.cpp

--- LogicOperators.cpp
+++ LogicOperators.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main()
 {
-    int year;
+    int year; // Define variable to store inputted year
     cout << "Enter the current year: ";
-    cin >> year;
+    cin >> year; // Store year from user input
 
-    bool isLeapYear = year % 4 == 0 && true || false;
-    cout << isLeapYear << endl;
+    bool isLeapYear = year % 4 == 0 && true || false; // Check if year is a leap year, modulo by 4
+    cout << isLeapYear << endl; // Print whether or not it is a leap year
 }
