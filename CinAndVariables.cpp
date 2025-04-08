ec6a6d599265c4b389c73d56523e67ca1dc3c044
Alex <108566298+okay-Alex@users.noreply.github.com>
2025-04-07 19:47:22 -0700

    Update CinAndVariables.cpp

--- CinAndVariables.cpp
+++ CinAndVariables.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
+    // Define variables to store user input for username and favoriteNumber
     string username;
     int favoriteNumber;
 
@@ -12,14 +13,17 @@ int main()
     cout << "Enter your favorite number: ";
     cin >> favoriteNumber;
 
+    // Combine username and favoriteNumber into one output string
     cout << "Your username is: " + username + ", and your favorite number is: " << favoriteNumber << "." << endl;
 
+    // Define variables of different types
     int secondFavoriteNumber = 7;
     float favoriteDecimal = 0.5;
     char firstInitial = 'A';
     string myName = "Alex";
     bool sleepy = false;
 
+    // Output variables
     cout << favoriteDecimal << endl;
     cout << secondFavoriteNumber << endl;
     cout << firstInitial << endl;
