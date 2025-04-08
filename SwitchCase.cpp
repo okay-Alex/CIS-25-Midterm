570e80dd8fd3a986c97f04ade04bc5645b452fe5
Alex <108566298+okay-Alex@users.noreply.github.com>
2025-04-07 19:42:03 -0700

    Add files via upload

--- SwitchCase.cpp
+++ SwitchCase.cpp
@@ -1,33 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    float firstNumber;
-    float secondNumber;
-    char operation;
+int main() {
+    int number = 10;
 
-    cout << "Enter your first number: ";
-    cin >> firstNumber;
-
-    cout << "Enter your second number: ";
-    cin >> secondNumber;
-
-    cout << "Enter the operation you'd like to perform: ";
-    cin >> operation;
-
-    switch (operation) {
-        case '+':
-            cout << firstNumber + secondNumber << endl;
-            break;
-        case '-':
-            cout << firstNumber - secondNumber << endl;
-            break;
-        case '*':
-            cout << firstNumber * secondNumber << endl;
-            break;
-        case '/':
-            cout << firstNumber / secondNumber << endl;
-            break;
+    if (number == 20) { // Changed = to ==, which is the proper logical operator to check for equivalence
+        cout << "Number is 20" << endl;
     }
+    else {
+        cout << "Number is not 20" << endl;
+    }
+
+    for (int i = 0; i <= 5; i++) { // Added curly bracket to begin block of code in for loop, and got rid of extra semicolon
+        cout << i << endl;
+    } // Added curly bracket to end block of code
+
+    return 0;
 }
